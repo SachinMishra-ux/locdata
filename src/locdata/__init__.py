617e683fba2bda37e29b784de3b571402e792621
Sachin Mishra <sachin19566@gmail.com>
2023-02-24 21:35:22 +0530

    init file changed

--- src/locdata/__init__.py
+++ src/locdata/__init__.py
@@ -1,3 +1,3 @@
-from locdata.timer import start, url_to_name, get_active_window, get_chrome_url
+from locdata.timer import start, get_active_window, get_chrome_url
 from locdata.analytics import json_data
 from locdata.charts import mydata_analysis
